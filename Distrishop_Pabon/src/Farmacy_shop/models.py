5856a764e9396c31717eab458d8103d1678c4bc2
Wichmann Alexander Saenz <wichmann.saenz@outlook.com>
2021-09-07 16:18:35 -0500

    montaje de infraestructura python

---
+++ Distrishop_Pabon/src/Farmacy_shop/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Proveedores(models.Model):
+    id=models.AutoField(primary_key=True)
+    codigo=models.CharField(max_length=10)
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=20)
+    telefono= models.IntegerField(max_length=22)
+    estado=models.BooleanField(("true"))
+    fecha_creacion=models.DateTimeField
+    fecha_modificacion=models.DateTimeField
+
+    
+
+class Productos(models.Model):
+    id =models.AutoField(primary_key= True)
+    nombres=models.CharField(max_length=30)
+    #id_proveedor=models.ForeignKey("app.Model", Proveedores=("id"), on_delete=models.CASCADE)
+    estado=models.BooleanField(("true"))    
+    fecha_creacion=models.DateTimeField
+    fecha_modificacion=models.DateTimeField
+
+
+
+
+
+class Stock(models.Model):
+    id=models.AutoField(primary_key=True)
+    codigo=models.CharField(max_length=10)
+    nombres=models.CharField(max_length=30)
+    apellidos=models.CharField(max_length=30)
+    cedula=models.CharField(max_length=15)
+    direccion=models.CharField(max_length=50)
+    fecha_creacion=models.DateTimeField
+    fecha_modificacion=models.DateTimeField
