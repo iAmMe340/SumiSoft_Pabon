2be4784a07367cb3345e6215b30f855162cf18eb
unknown <elkin_9078@hotmail.com>
2021-10-27 12:29:45 +0200

    adicion de vistas y rutas de farmacia e in

--- Distrishop_Pabon/src/SumiSoft_Pabon/urls.py
+++ Distrishop_Pabon/src/SumiSoft_Pabon/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('SumFarmacia/', include('SuministrosFarmacia.urls'))
+    path('SuministrosFarmacia/', include('SuministrosFarmacia.urls'))
   
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
