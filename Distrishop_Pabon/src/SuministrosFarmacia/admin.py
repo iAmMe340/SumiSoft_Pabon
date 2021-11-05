9c8a2cd78d3d18416552aa50d57422db29082fd7
iAmMe340 <elkin_9078@hotmail.com>
2021-11-04 18:14:48 -0600

    montaje bootstrap

--- Distrishop_Pabon/src/SuministrosFarmacia/admin.py
+++ Distrishop_Pabon/src/SuministrosFarmacia/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Productos)
+admin.site.register(Existencias)
 admin.site.register(Proveedores)
+admin.site.register(Portafolio)
+admin.site.register(Mejorprecio)
 admin.site.register(Stock)
+admin.site.register(Cotizaciones)
--- Distrishop_Pabon/src/SuministrosFarmacia/models.py
+++ Distrishop_Pabon/src/SuministrosFarmacia/models.py
@@ -1,34 +1,63 @@
 from django.db import models
 
 # Create your models here.
-class Proveedores(models.Model):
-    id=models.AutoField(primary_key=True)
-    codigo=models.CharField(max_length=10)
-    nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=20)
-    telefono= models.IntegerField(max_length=22)
-    estado=models.BooleanField(("true"))
-    fecha_creacion=models.DateTimeField
-    fecha_modificacion=models.DateTimeField
+class Proveedores(models.Model): #Tabla de Proveedores
+    codigo_empresa = models.CharField(max_length=10, primary_key=True, default=0) 
+    nombre = models.CharField(max_length=50, default='')
+    direccion = models.CharField(max_length=50, default='')
+    telefono = models.CharField(max_length=50, default='')
+    email = models.EmailField(max_length=50, blank=True, default='')
 
-    
+    def __str__(self):
+        return self.codigo_empresa, self.nombre, self.direccion, self.telefono, self.email
 
-class Productos(models.Model):
-    id =models.AutoField(primary_key= True)
-    nombres=models.CharField(max_length=30)
-    #id_proveedor=models.ForeignKey("app.Model", Proveedores=("id"), on_delete=models.CASCADE)
-    estado=models.BooleanField(("true"))    
-    fecha_creacion=models.DateTimeField
-    fecha_modificacion=models.DateTimeField
-
+class Existencias(models.Model):#tabla de existencias
+    codigo_producto = models.CharField(max_length=10, primary_key=True, default='')
+    descripcion = models.CharField(max_length=250, default='')
+    cantidad = models.IntegerField(default=0)
+    fecha_ingreso = models.DateField()
+    fecha_vencimiento = models.DateField()
+    def __str__(self):
+        return self.codigo_producto, self.descripcion, self.cantidad, self.fecha_vencimiento
 
 
-class Stock(models.Model):
-    id=models.AutoField(primary_key=True)
-    codigo=models.CharField(max_length=10)
-    nombres=models.CharField(max_length=30)
-    apellidos=models.CharField(max_length=30)
-    cedula=models.CharField(max_length=15)
-    direccion=models.CharField(max_length=50)
-    fecha_creacion=models.DateTimeField
-    fecha_modificacion=models.DateTimeField
+class Portafolio(models.Model): #tabla deProductos
+    codigo_empresa = models.ForeignKey(Proveedores, on_delete=models.CASCADE, null=True, blank=True)
+    codigo_producto = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+    precio = models.FloatField()
+    stock = models.BooleanField(default=True, blank=True, null=True, verbose_name='Disponible', help_text='Disponible',)
+
+    def __str__(self):
+        return self.codigo_empresa, self.codigo_producto , self.nombre, self.descripcion, self.precio
+
+
+
+class Stock(models.Model): #tabla de stock
+   
+    id_producto = models.ForeignKey(Existencias, on_delete=models.CASCADE, null=True, blank=True)
+    descripcion = models.CharField(max_length=250, default='')
+    stockmax = models.IntegerField(default=0)
+    stockmin = models.IntegerField(default=0)
+    stockrecomendado = models.IntegerField(default=0)
+    fecha_ingreso = models.DateField(default= '2020-01-01')
+    fecha_vencimiento = models.DateField(default= '2020-01-01')
+    def __str__(self):
+        return self.id_producto, self.descripcion, self.stockmax, self.stockmin, self.stockrecomendado, self.fecha_vencimiento
+
+
+class Mejorprecio(models.Model): #tabla de mejores precios
+    id_proveedor = models.ForeignKey(Proveedores, on_delete=models.CASCADE)
+    id_producto = models.ForeignKey(Existencias, on_delete=models.CASCADE)
+    precio_minimo = models.FloatField(default=0)
+    def __str__(self):
+        return self.id_proveedor, self.id_producto, self.precio_minimo
+
+
+class Cotizaciones(models.Model): #tabla de cotizaciones
+    id_producto = models.ForeignKey(Existencias, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=250)
+    precio = models.FloatField(default=0)
+    def __str__(self):
+        return self.id_proveedor, self.id_producto, self.precio
